6d2ff6ece262adcedc4616fa473a444fe7b9b40e
Xavier Taylor <xaviermt1@gmail.com>
2021-04-08 20:03:53 +1000

    added other corpuses to um corpus script

--- nlp/ingest_corpus.py
+++ nlp/ingest_corpus.py
@@ -61,7 +61,11 @@ Reference
 [1] Liang Tian, Derek F. Wong, Lidia S. Chao, Paulo Quaresma, Francisco Oliveira, Shuo Li, Yiming Wang, Yi Lu, "UM-Corpus: A Large English-Chinese Parallel Corpus for Statistical Machine Translation". Proceedings of the 9th International Conference on Language Resources and Evaluation (LREC'14), Reykjavik, Iceland, 2014.'''
 um_ed_summary = 'The texts in this domain are acquired from online  teaching  materials,  such  as  language  teaching resources,  dictionaries,  etc.,  which  can  be  served  as language education'
 
-
+# I just so happened to ingest the 8 corpora (+ 1 test set) in these chunks, no significance in their order etc
+# A done, B and C tbd
+#sub_corpora_a=[SubCorpus(title='Education', path='Education/Bi-Education.txt', summary=um_ed_summary)])
+sub_corpora_b = [SubCorpus(title='Laws', path='Laws/Bi-Laws.txt', summary='TBD'), SubCorpus(title='Microblog', path='Microblog/Bi-Microblog.txt', summary='TBD'), SubCorpus(title='News', path='News/Bi-News.txt', summary='TBD'), SubCorpus(title='Spoken', path='Spoken/Bi-Spoken.txt', summary='TBD') ]
+#sub_corpora_c = [SubCorpus(title='Science', path='Science/Bi-Science.txt', summary='TBD'), SubCorpus(title='Subtitles', path='Subtitles/Bi-Subtitles.txt', summary='TBD'), SubCorpus(title='Thesis', path='Thesis/Bi-Thesis.txt', summary='TBD'), SubCorpus(title='Testing', path='Testing/Testing-Data.txt', summary='Test data for the um corpus') ]
 
 corpus = Corpus(
     title='UM-Corpus', 
@@ -69,8 +73,8 @@ corpus = Corpus(
     licence='https://creativecommons.org/licenses/by-nc-nd/4.0/',
     website='http://nlp2ct.cis.umac.mo/um-corpus/',
     summary=um_summary,
-    sub_corpuses=[SubCorpus(title='Education', path='Education/Bi-Education.txt', summary=um_ed_summary)])
-
+    #sub_corpuses=[SubCorpus(title='Education', path='Education/Bi-Education.txt', summary=um_ed_summary)])
+    sub_corpuses=sub_corpora_b)
 
 zh_nlp = stanza.Pipeline('zh')
 # records = cur.fetchall()
